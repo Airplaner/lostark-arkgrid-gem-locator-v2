90306309cb7bfac5e554b367b3b99f3f76a299da
Airplaner <16368653+Airplaner@users.noreply.github.com>
2025-10-24 12:16:00 +0900

    feat/코어 계수 구조 변경 및 디자인 추가

--- src/components/ArkGridCoreAddPanel.svelte
+++ src/components/ArkGridCoreAddPanel.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
   import { ArkGridAttr, ArkGridGrade } from '../lib/constants/enums';
-  import { ArkGridCoreType, createCore, getDefaultCoreCoeff } from '../lib/models/arkGridCores';
+  import { ArkGridCoreType, createCore, resetCoreCoeff } from '../lib/models/arkGridCores';
   import { arkGridCores, initArkGridCores } from '../stores/store';
-  import ArkgridGemAddPanel from './ArkGridGemAddPanel.svelte';
 
   const attrs = Object.values(ArkGridAttr);
   const ctypes = Object.values(ArkGridCoreType);
   const grades = Object.values(ArkGridGrade);
+  const coeffKeys = ['p10', 'p14', 'p17', 'p18', 'p19', 'p20'] as const;
 
   function createNewCore(attr: ArkGridAttr, ctype: ArkGridCoreType) {
     // 코어가 없을 때 새로운 코어 추가
@@ -22,59 +22,55 @@
     // arkGridCores.reset(); // 이상하게 안 됨
     return;
   }
-  function resetCoeffWhenGradeChanges(
-    attr: ArkGridAttr,
-    ctype: ArkGridCoreType,
-    grade: ArkGridGrade
-  ) {
+  function resetCoeffWhenGradeChanges(attr: ArkGridAttr, ctype: ArkGridCoreType) {
     arkGridCores.update((cores) => {
       const core = cores[attr][ctype];
       if (!core) return cores;
-      core.coeff = getDefaultCoreCoeff(attr, ctype, grade);
+      resetCoreCoeff(core);
       return cores;
     });
   }
 </script>
 
 <div class="panel">
-  <button on:click={resetAllCores}>초기화</button>
+  <button onclick={resetAllCores}>초기화</button>
   {#each attrs as attr}
     {#each ctypes as ctype}
       <div class="core-slot">
-        <div class="title">
+        <div class="row core-name">
           {attr}의 {ctype} 코어
         </div>
 
         {#if $arkGridCores[attr][ctype]}
-          <div class="core-info">
-            <div class="grade">
-              <span class="title">코어 등급</span>
-              {#each grades as grade}
-                <label>
-                  <input
-                    type="radio"
-                    name="{attr} {ctype} grade"
-                    bind:group={$arkGridCores[attr][ctype].grade}
-                    on:change={() => resetCoeffWhenGradeChanges(attr, ctype, grade)}
-                    value={grade}
-                  />
-                  {grade}
-                </label>
-              {/each}
-            </div>
+          <div class="row core-grade">
+            <span class="title">코어 등급</span>
+            {#each grades as grade}
+              <label>
+                <input
+                  type="radio"
+                  name="{attr} {ctype} grade"
+                  bind:group={$arkGridCores[attr][ctype].grade}
+                  onchange={() => resetCoeffWhenGradeChanges(attr, ctype, grade)}
+                  value={grade}
+                />
+                {grade}
+              </label>
+            {/each}
+          </div>
 
-            <div class="coeff">
-              <span class="title">계수</span>
-              {#each [10, 14, 17, 18, 19, 20] as i}
-                {i}P:
-                <label>
-                  <input class="core-coeff" bind:value={$arkGridCores[attr][ctype].coeff[i]} />
-                </label>
-              {/each}
-            </div>
+          <div class="row core-coeffs">
+            <span class="title">계수</span>
+            {#each coeffKeys as coeffKey}
+              <label class="core-coeff">
+                {coeffKey.slice(1)}
+                <input type="number" bind:value={$arkGridCores[attr][ctype].coeffs[coeffKey]} />
+              </label>
+            {/each}
           </div>
         {:else}
-          <button on:click={() => createNewCore(attr, ctype)}>+</button>
+          <div class="row">
+            <button onclick={() => createNewCore(attr, ctype)}>+</button>
+          </div>
         {/if}
       </div>
     {/each}
@@ -82,7 +78,33 @@
 </div>
 
 <style>
-  .panel .core-coeff {
-    width: 30px;
+  .core-slot {
+    border: 1px solid black;
+    padding: 0.5rem;
+  }
+  .core-slot > .core-name {
+    font-weight: 700;
+  }
+  .core-slot > .row {
+    display: flex;
+    flex-wrap: nowrap;
+    align-items: center;
+  }
+
+  .core-slot > .row > .title {
+    font-weight: 500;
+    min-width: 5em;
+  }
+  .core-slot > .core-coeffs {
+    gap: 0.8rem;
+    flex-wrap: wrap;
+  }
+  .core-slot > .core-coeffs > .core-coeff{
+    display: flex;
+    flex-wrap: nowrap;
+    gap: 0.2em;
+  }
+  .core-slot > .core-coeffs > .core-coeff > input {
+    width: 3rem;
   }
 </style>
--- src/lib/models/arkGridCores.ts
+++ src/lib/models/arkGridCores.ts
@@ -6,67 +6,88 @@ export enum ArkGridCoreType {
   STAR = '별',
 }
 
+type ArkGridCoreCoeffs = {
+  p10: number;
+  p14: number;
+  p17: number;
+  p18: number;
+  p19: number;
+  p20: number;
+};
+
 export interface ArkGridCore {
   attr: ArkGridAttr;
   type: ArkGridCoreType;
   grade: ArkGridGrade;
-  coeff: Array<number>;
+  coeffs: ArkGridCoreCoeffs;
 }
 
-function getFullCoeff(p10: number, p14: number, p17: number, grade: ArkGridGrade): Array<number> {
-  // 0p-9p는 모두 0
-  const coeff = Array(10).fill(0);
-  // 10p-13p는 10p 값 기준
-  for (let i = 0; i < 4; i++) coeff.push(p10);
-  // 14p-16p는 14p 값 기준
-  for (let i = 0; i < 3; i++) coeff.push(p14);
-  // 고대라면 17p에 +100
-  if (grade == ArkGridGrade.ANCIENT) p17 += 100;
-  coeff.push(p17);
-  coeff.push(p17 + 17);
-  coeff.push(p17 + 33);
-  coeff.push(p17 + 50);
-
-  // 영웅이라면 14p 이상 값 평탄화
-  if (grade == ArkGridGrade.EPIC) {
-    for (let i = 14; i <= 20; i++) {
-      coeff[i] = coeff[10];
-    }
-  }
-
-  // 전설이라면 17p 이상 값 평탄화
-  else if (grade == ArkGridGrade.LEGENDARY) {
-    for (let i = 17; i <= 20; i++) {
-      coeff[i] = coeff[14];
-    }
-  }
-  return coeff;
+export function resetCoreCoeff(core: ArkGridCore) {
+  core.coeffs = getDefaultCoreCoeff(core.attr, core.type);
+  adjustCoeff(core);
 }
 
-export function getDefaultCoreCoeff(
-  attr: ArkGridAttr,
-  type: ArkGridCoreType,
-  grade: ArkGridGrade
-): Array<number> {
+function adjustCoeff(core: ArkGridCore) {
+  // 코어 등급에 따라 계수 조정
+  if (core.grade == ArkGridGrade.EPIC) {
+    // 영웅 등급: 10P 옵션까지만 존재
+    core.coeffs.p14 = core.coeffs.p10;
+    core.coeffs.p17 = core.coeffs.p10;
+    core.coeffs.p18 = core.coeffs.p10;
+    core.coeffs.p19 = core.coeffs.p10;
+    core.coeffs.p20 = core.coeffs.p10;
+  } else if (core.grade == ArkGridGrade.LEGENDARY) {
+    // 전설 등급 : 14P 옵션까지만 존재
+    core.coeffs.p17 = core.coeffs.p14;
+    core.coeffs.p18 = core.coeffs.p14;
+    core.coeffs.p19 = core.coeffs.p14;
+    core.coeffs.p20 = core.coeffs.p14;
+  } else if (core.grade == ArkGridGrade.ANCIENT) {
+    // 고대 등급 : 17-20P 옵션 계수 +100
+    core.coeffs.p17 += 100;
+    core.coeffs.p18 += 100;
+    core.coeffs.p19 += 100;
+    core.coeffs.p20 += 100;
+  }
+}
+
+export function getDefaultCoreCoeff(attr: ArkGridAttr, type: ArkGridCoreType): ArkGridCoreCoeffs {
   if (attr == ArkGridAttr.Order) {
     if (type == ArkGridCoreType.SUN || type == ArkGridCoreType.MOON) {
-      return getFullCoeff(150, 400, 750, grade);
+      return {
+        p10: 150,
+        p14: 400,
+        p17: 750,
+        p18: 767,
+        p19: 783,
+        p20: 800,
+      };
     } else if (type == ArkGridCoreType.STAR) {
-      return getFullCoeff(100, 250, 450, grade);
+      return {
+        p10: 100,
+        p14: 250,
+        p17: 450,
+        p18: 467,
+        p19: 483,
+        p20: 500,
+      };
     }
   }
-  return [];
+  return {
+    p10: 0,
+    p14: 0,
+    p17: 0,
+    p18: 0,
+    p19: 0,
+    p20: 0,
+  };
 }
 
 export function createCore(
-  attr: ArkGridAttr,
-  type: ArkGridCoreType,
-  grade: ArkGridGrade
+  attr: ArkGridAttr, type: ArkGridCoreType, grade: ArkGridGrade,
 ): ArkGridCore {
-  return {
-    attr,
-    type,
-    grade,
-    coeff: getDefaultCoreCoeff(attr, type, grade),
-  };
+  const coeffs = getDefaultCoreCoeff(attr, type);
+  const core: ArkGridCore = {attr, type, grade, coeffs};
+  adjustCoeff(core);
+  return core;
 }
