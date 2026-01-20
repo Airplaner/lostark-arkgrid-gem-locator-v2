45b0ada9b7119571db7b901a1542ea4682613596
Airplaner <16368653+Airplaner@users.noreply.github.com>
2026-01-20 17:32:24 +0900

    feat/장착된 젬들의 옵션 합 추가

--- src/components/CoreGemEquipped.svelte
+++ src/components/CoreGemEquipped.svelte
@@ -3,7 +3,10 @@
     type ArkGridCore,
     getDefaultCoreEnergy,
   } from '../lib/models/arkGridCores';
-  import type { ArkGridGem } from '../lib/models/arkGridGems';
+  import {
+    type ArkGridGem,
+    ArkGridGemOptionTypes,
+  } from '../lib/models/arkGridGems';
   import ArkGridGemDetail from './ArkGridGemDetail.svelte';
 
   let { core, gems }: { core: ArkGridCore | null; gems: ArkGridGem[] } =
@@ -19,6 +22,45 @@
       return sum + gem.req;
     }, 0);
   });
+  let totalAtt = $derived.by(() => {
+    return gems.reduce((sum, gem) => {
+      return (
+        sum +
+        (gem.option1.optionType == ArkGridGemOptionTypes.ATTACK
+          ? gem.option1.value
+          : 0) +
+        (gem.option2.optionType == ArkGridGemOptionTypes.ATTACK
+          ? gem.option2.value
+          : 0)
+      );
+    }, 0);
+  });
+  let totalSkill = $derived.by(() => {
+    return gems.reduce((sum, gem) => {
+      return (
+        sum +
+        (gem.option1.optionType == ArkGridGemOptionTypes.SKILL_DAMAGE
+          ? gem.option1.value
+          : 0) +
+        (gem.option2.optionType == ArkGridGemOptionTypes.SKILL_DAMAGE
+          ? gem.option2.value
+          : 0)
+      );
+    }, 0);
+  });
+  let totalBoss = $derived.by(() => {
+    return gems.reduce((sum, gem) => {
+      return (
+        sum +
+        (gem.option1.optionType == ArkGridGemOptionTypes.BOSS_DAMAGE
+          ? gem.option1.value
+          : 0) +
+        (gem.option2.optionType == ArkGridGemOptionTypes.BOSS_DAMAGE
+          ? gem.option2.value
+          : 0)
+      );
+    }, 0);
+  });
 </script>
 
 <div class="root">
@@ -30,6 +72,11 @@
   <div>
     <div>포인트: {corePoint}P</div>
     <div>의지력: {usedPower}/{getDefaultCoreEnergy(core)}</div>
+    <div hidden>
+      <p>공격력: {totalAtt}</p>
+      <p>추가 피해: {totalSkill}</p>
+      <p>보스 피해: {totalBoss}</p>
+    </div>
   </div>
 
   <div class="gems">
