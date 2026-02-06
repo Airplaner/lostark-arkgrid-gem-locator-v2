9ae1f7b6e3772bdfada111f6f944e02b23e04dff
Airplaner <airplaner@users.noreply.github.com>
2026-02-07 00:45:19 +0900

    style/minor fix on english GemDetail

--- src/components/ArkGridGemDetail.svelte
+++ src/components/ArkGridGemDetail.svelte
@@ -22,30 +22,30 @@
     </div>
 
     <div class="willPower gem-spec">
-      <div>{gem.req}</div>
+      <div class="text">{gem.req}</div>
       <img src={imgWillPower} alt="W" />
     </div>
 
     <div class="vl"></div>
 
     <div class="option1 gem-spec">
-      <div class="shrinkable">
+      <div class="text shrinkable">
         {ArkGridGemOptionTypes[gem.option1.optionType].name[locale]}
       </div>
-      <div>
+      <div class="text">
         Lv. {gem.option1.value}
       </div>
     </div>
 
     <div class="corePoint gem-spec">
-      <div>
+      <div class="text">
         {gem.point}
       </div>
       <img src={imgCorePoint} alt="P" />
     </div>
 
     <div class="option2 gem-spec">
-      <div class="shrinkable">
+      <div class="text shrinkable">
         {ArkGridGemOptionTypes[gem.option2.optionType].name[locale]}
       </div>
       <div>
@@ -95,8 +95,18 @@
     height: 100%;
   }
   @container (max-width: 300px) {
+    /* CoreGemEquipped 전용 css */
+    /* 영문 버전 글자가 많아서, vertical line 제거 및 약간의 margin으로 대칭 */
     .gem[data-locale='en_us'] {
       column-gap: 0.3rem;
+      grid-template-columns: 2.5rem 2rem auto;
+    }
+    .gem[data-locale='en_us'] > .vl {
+      display: none;
+      height: 0%;
+    }
+    .gem[data-locale='en_us'] > .gem-spec {
+      margin-left: 0.1rem;
     }
   }
   /* 두 칸씩 먹는 이미지와 세로선 */
@@ -124,6 +134,10 @@
     white-space: nowrap;
     overflow: hidden;
   }
+  .gem > .gem-spec > .text {
+    /* gem-spec안의 글자들이 윗공간이 남아서 살짝 올림 */
+    transform: translateY(-0.075rem);
+  }
 
   img {
     object-fit: contain;
@@ -134,9 +148,7 @@
     transform: translateX(0.05rem);
   }
   .gem-spec > img {
-    /* corepoint, willpower는 아래로 1px */
     height: 80%;
-    transform: translateY(0.05rem);
   }
 
   .shrinkable {
